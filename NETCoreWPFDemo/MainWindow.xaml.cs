2e096575aa05db147f2273d9398a3994142a8753
suresh-mohan <sureforall@gmail.com>
2019-05-06 11:05:16 +0530

    Added the project into the repository.

---
+++ NETCoreWPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using Syncfusion.UI.Xaml.Grid;
+
+namespace NETCoreWPFDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SfDataGrid dataGrid = new SfDataGrid() { Height = 550, Width = 600, ColumnSizer = GridLengthUnitType.Star };
+            dataGrid.ItemsSource = GetDataTable();
+            this.ROOT_Grid.Children.Add(dataGrid);
+        }
+
+        public DataTable GetDataTable()
+        {
+            DataTable employeeCollection = new DataTable();
+            var dt = DateTime.Now;
+
+            employeeCollection.Columns.Add("EmployeeID", typeof(int));
+            employeeCollection.Columns[0].ColumnName = "Employee ID";
+            employeeCollection.Columns.Add("EmployeeName", typeof(string));
+            employeeCollection.Columns["EmployeeName"].ColumnName = "Employee Name";
+            employeeCollection.Columns.Add("CustomerID", typeof(string));
+            employeeCollection.Columns["CustomerID"].ColumnName = "Customer ID";
+            employeeCollection.Columns.Add("Country", typeof(string));
+            employeeCollection.Columns.Add("Date", typeof(DateTime));
+
+            employeeCollection.Rows.Add(1001, "Belgim", "YHGTR", "US", new DateTime(dt.Year, dt.Month, dt.Day));
+            employeeCollection.Rows.Add(1002, "Oliver", "LDON", "UK", new DateTime(dt.Year, dt.Month, dt.AddDays(-1).Day));
+            employeeCollection.Rows.Add(1003, "Bernald", "ALFKI", "US", new DateTime(dt.Year, dt.Month, dt.AddDays(-5).Day));
+            employeeCollection.Rows.Add(1004, "James", "YHGTR", "US", new DateTime(dt.Year, dt.Month, dt.AddDays(-1).Day));
+            employeeCollection.Rows.Add(1005, "Beverton", "BERGS", "Europe", new DateTime(dt.Year, dt.Month, dt.Day));
+
+            return employeeCollection;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# .net-core-3.0-wpf-demo
-This repository contains the project that uses Syncfusion WPF DataGrid control to display simple information in a .NET Core 3.0 application.
+# Syncfusion WPF DataGrid control used in .NET Core 3.0 application
+
+This repository contains the examples of usage of [Syncfusion WPF DataGrid control](https://www.syncfusion.com/wpf-ui-controls/datagrid) a .NET Core 3.0 application.
+
+## Getting Started
+
+* Checkout this project to a location in your disk.
+* Running the project should restore the required nuget packages and deploy the same.
+
+### Prerequisites
+
+The prerequisites for running a .NET Core Core project are:
+* Visual Studio 2019
+* .NET Core 3.0 SDK (Download Link)
